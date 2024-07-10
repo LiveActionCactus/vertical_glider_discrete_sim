229b8ebbd92a317e05261a15976dc0a58e624a45
Patrick Ledzian <patrick.ledzian@live.com>
2024-07-09 20:03:33 -0400

    trying to plot the time-varying comms graph but having issues. The nodes keep jumping around, it doesn't look like agent 3 is ever in comms (need to double check node #, i'm not displaying node labels..), and it takes a long time to run and dump a gif file.

--- Plotting.py
+++ Plotting.py
@@ -1,45 +1,117 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import networkx as nx
+import itertools
+from matplotlib import animation
+import math
 
 class Plotting():
 
 	def __init__(self, sim_env):
-		self._sim_env = sim_env 
+		self._sim_env = sim_env
+		self._animation_path = './animations/10agents_200min_waituntil.gif'
+
 
 	def plot_state(self):
-		k = np.linspace(0, self._sim_env._tmax/60, self._sim_env._t)		# defines the simulation length and step size
+		k_ = np.linspace(0, self._sim_env._tmax/60, self._sim_env._t)		# defines the simulation length and step size
 
-		fig, axs = plt.subplots(2,2)
+		fig_, axs_ = plt.subplots(2,2)
 		
 		for i in range(0, self._sim_env._num_agents):
-			agent = self._sim_env._state_log[:,:,i]
+			agent_ = self._sim_env._state_log[:,:,i]
 
 			# plot positions
-			axs[0,0].plot(k, agent[0,:], label='Agent {0}'.format(i))
-			axs[0,0].set(xlabel='Time (min)', ylabel='Depth (m)')
-			axs[0,0].set_title('Position vs. Time')
+			axs_[0,0].plot(k_, agent_[0,:], label='Agent {0}'.format(i))
+			axs_[0,0].set(xlabel='Time (min)', ylabel='Depth (m)')
+			axs_[0,0].set_title('Position vs. Time')
 			if self._sim_env._num_agents < 5:
-				axs[0,0].legend()
+				axs_[0,0].legend()
 
 			# plot velocities
-			axs[0,1].plot(k, agent[1,:], label='Agent {0}'.format(i))
-			axs[0,1].set(xlabel='Time (min)', ylabel='Velocity (m/s)')
-			axs[0,1].set_title('Velocity vs. Time')
+			axs_[0,1].plot(k_, agent_[1,:], label='Agent {0}'.format(i))
+			axs_[0,1].set(xlabel='Time (min)', ylabel='Velocity (m/s)')
+			axs_[0,1].set_title('Velocity vs. Time')
 			if self._sim_env._num_agents < 5:
-				axs[0,1].legend()
+				axs_[0,1].legend()
 
 			# plot ballast
-			axs[1,0].plot(k, agent[2,:], label='Agent {0}'.format(i))
-			axs[1,0].set(xlabel='Time (min)', ylabel='Ballast (kg)')
-			axs[1,0].set_title('Ballast vs. Time')
+			axs_[1,0].plot(k_, agent_[2,:], label='Agent {0}'.format(i))
+			axs_[1,0].set(xlabel='Time (min)', ylabel='Ballast (kg)')
+			axs_[1,0].set_title('Ballast vs. Time')
 			# if self._sim_env._num_agents < 5:
-			axs[1,0].legend()
+			axs_[1,0].legend()
 
 			# plot phase
-			axs[1,1].plot(k, agent[3,:], label='Agent {0}'.format(i))
-			axs[1,1].set(xlabel='Time (min)', ylabel='Phase (rad)')
-			axs[1,1].set_title('Phase vs. Time')
+			axs_[1,1].plot(k_, agent_[3,:], label='Agent {0}'.format(i))
+			axs_[1,1].set(xlabel='Time (min)', ylabel='Phase (rad)')
+			axs_[1,1].set_title('Phase vs. Time')
 			if self._sim_env._num_agents < 5:
-				axs[1,0].legend()
+				axs_[1,0].legend()
 
-		plt.show(block="True")
+		plt.show(block="False")
+
+
+	def plot_time_varying_graph(self):
+		# https://stackoverflow.com/questions/31815454/animate-graph-diffusion-with-networkx
+
+		on_surface_log_ = self._sim_env.comms._on_surface_log
+		multiplier_ = 23
+
+		nodes_ = np.arange(0, on_surface_log_.shape[1], 1)  		# generate array of nodes/agents, {0, ... N}
+		masked_ = np.multiply(nodes_, on_surface_log_[0,:]) 			# init graph state
+		masked_ = masked_[masked_ != 0]
+		edges_ = itertools.combinations(masked_,2) 					# generate edges at init graph state
+
+		G = nx.Graph() 												# initialize the graph
+		G.add_nodes_from(nodes_)
+		G.add_edges_from(edges_)
+
+		fig_, ax_ = plt.subplots(figsize=(6,4))
+
+		layout = nx.circular_layout(G) 								# init graph layout; faster to pass the pre-built layout
+
+		ani = animation.FuncAnimation(fig_, self.simple_update, frames=math.floor(on_surface_log_.shape[0]/multiplier_), fargs=(multiplier_, layout, G, on_surface_log_, nodes_, ax_))
+		ani.save(self._animation_path, writer='pillow', fps=30)
+
+
+	# def plot_graph_edges_as_random(self):
+
+
+	#####
+	# HELPER FUNCTIONS
+	#####
+
+	def complete_graph_from_list(self, L, create_using=None):
+	    G = nx.empty_graph(len(L),create_using)
+	    if len(L)>1:
+	        if G.is_directed():
+	            edges = itertools.permutations(L,2)
+	        else:
+	            edges = itertools.combinations(L,2)
+	        G.add_edges_from(edges)
+
+	    return G
+
+	# TODO: graph is jumping around; find a way to remove edges but not the nodes
+	# TODO: why is agent3 never in comms? check the data vector
+	def simple_update(self, num, multiplier, layout, G, on_surface_log_, nodes_, ax):
+		itr = math.floor(num*multiplier)
+
+		ax.clear()
+
+		# G.clear()   																			# TODO: need a better solution; don't delete the nodes
+		# G.add_nodes_from(nodes_)
+		edges_ = self.recover_graph_edges(nodes_, on_surface_log_[itr,:])		# determine new edges
+		# G.add_edges_from(edges_)
+		G.update(edges=edges_)
+
+		nx.draw(G, pos=layout, ax=ax, with_labels=True) 						# draw the graph
+		ax.set_title("Frame {}".format(itr)) 	# update the title
+
+
+	def recover_graph_edges(self, nodes, itr_edges):
+		masked_ = np.multiply(nodes, itr_edges)
+		masked_ = masked_[masked_ != 0]
+		edges_ = itertools.combinations(masked_,2)
+
+		return edges_
--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 from SimEnv import SimEnv
 from Plotting import Plotting
+import pickle
 
+
+data_path = './data/GOOD_10agents_200min_waituntil.pkl'
+
+def pickle_results(sim):
+	sim_ = sim
+	state_log_ = sim._state_log
+	on_surface_log_ = sim.comms._on_surface_log
+	data = [sim_, state_log_, on_surface_log_]
+
+	with open(data_path, 'wb') as f:
+		pickle.dump(data, f)
+	f.close
+
+def load_pickle_results():
+	with open(data_path, 'rb') as f:
+		data = pickle.load(f)
+	f.close()
+
+	return data
 
 if __name__ == "__main__":
-	sim = SimEnv();
+	# sim = SimEnv();
 
-	sim.run_sim()
+	# sim.run_sim()
+	# pickle_results(sim)
+
+	data = load_pickle_results()
+	sim = data[0]
 
 	plt = Plotting(sim)
-	plt.plot_state()
+	plt.plot_time_varying_graph()
+	# plt.plot_state()
